d8225cadb6390b46732e113ca1c418d0d36eeb61
master <master.code.develop@gmail.com>
2022-06-10 00:13:23 +0200

    backend | app

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
-const path = require('path');
+// IMPORTS
+const express = require('express'),
+bodyParser = require('body-parser'),
+path = require('path'),
+sauceRoutes = require('./routes/sauce'),
+authRoutes = require('./routes/auth'),
+app = express();
 
 require('dotenv').config();
 require('./db/mongoose')
 
-const sauceRoutes = require('./routes/sauce');
-const authRoutes = require('./routes/auth');
-
-
-const app = express();
 
 app.use((req, res, next) => {
+    // Permet d'accéder l'API depuis n'importe quelle origine ('*')
     res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Permet d'ajouter les headers mentionnés aux requêtes envoyées vers l'API
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    
+    // Permet d'envoyer des requêtes avec les méthodes mentionnées
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    // Renvoie au prochain Middleware
     next();
 });
 
 app.use(bodyParser.json());
 
+// Enregistrement du 'router' pour toutes les demandes effectuées
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', authRoutes);
 
+//  exports
 module.exports = app;
